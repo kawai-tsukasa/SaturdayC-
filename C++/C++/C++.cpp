f5242a99dbc2deeadc8fb6ac56422f8bc3e9d865
XPS\ktc <tebasaki0705@icloud.com>
2026-04-25 14:44:52 +0900

    aaa

---
+++ C++/C++/C++.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	/*float num; //変数の宣言(int = 整数 float = 小数)
+
+		num = 3.14159265358979323846264339;//変数の代入
+
+    printf("%f\n",num);
+
+	return 0; 
+	*/
+	
+    /*
+	//変数を宣言する
+	float num;
+
+	//変数「num」にキーボードで押した数値を代入する
+	scanf_s("%f", &num);
+
+	//numに代入されている値を表示する
+	printf("入力した値は%fです", num);
+	return 0;
+	*/
+
+	/*
+	char c = 'a';
+	printf("%c\n", c);
+	*/
+
+
+	/*
+	int a = 1;
+	int b = 2;
+
+	float answer = (float)a / (float)b;
+	printf("%f\n", answer);
+    */
+
+	/*
+	//　ローカル変数
+	{
+		int a = 1;
+		{
+			a = 2;//　エラーにならない
+		}
+	}
+
+	a = 2;// こちらはエラー
+
+	*/
+
+	// const = 更新できない変数
+
+
+	return 0;
+}
